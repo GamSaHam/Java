52ef047d9f357e8a3b31535893a52ef17e7afa6d
gsham <gsham@12cm.co.kr>
2020-03-26 17:58:37 +0900

    1

---
+++ StreamExam/src/main/java/Employee.java
@@ -0,0 +1,43 @@
+
+
+
+public class Employee {
+    private Long id;
+    private String name;
+    private double salary;
+
+
+    public Employee(Long id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void salaryIncrement(double salary){
+        this.salary += salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
